88e864703dc43a9e21c18023a63afddf64c61a1a
DESKTOP-JQ27UAA\dyco <mystyle@kiss99.com>
2019-09-26 01:12:27 +0800

    add jwt long expire and refresh key

---
+++ Vue.Core.Data/Migrations/20190925032031_init.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Vue.Core.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    AppName = table.Column<string>(maxLength: 20, nullable: true),
+                    ParentId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apps", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    PatientId = table.Column<string>(maxLength: 30, nullable: true),
+                    NationalId = table.Column<string>(maxLength: 30, nullable: true),
+                    FirstName = table.Column<string>(maxLength: 30, nullable: true),
+                    LastName = table.Column<string>(maxLength: 30, nullable: true),
+                    Email = table.Column<string>(maxLength: 100, nullable: true),
+                    Gender = table.Column<int>(nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "Date", nullable: true),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    RoleName = table.Column<string>(maxLength: 20, nullable: true),
+                    Lock = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    LoginName = table.Column<string>(maxLength: 20, nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true),
+                    FirstName = table.Column<string>(maxLength: 30, nullable: true),
+                    LastName = table.Column<string>(maxLength: 30, nullable: true),
+                    Email = table.Column<string>(maxLength: 100, nullable: true),
+                    Gender = table.Column<int>(nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "Date", nullable: true),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppsApiCollection",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    RoutePath = table.Column<string>(nullable: true),
+                    AppsId = table.Column<int>(nullable: true),
+                    Hits = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppsApiCollection", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppsApiCollection_Apps_AppsId",
+                        column: x => x.AppsId,
+                        principalTable: "Apps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolesApps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Read = table.Column<bool>(nullable: false),
+                    Write = table.Column<bool>(nullable: false),
+                    Print = table.Column<bool>(nullable: false),
+                    Delete = table.Column<bool>(nullable: false),
+                    RolesId = table.Column<int>(nullable: true),
+                    AppsId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolesApps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RolesApps_Apps_AppsId",
+                        column: x => x.AppsId,
+                        principalTable: "Apps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RolesApps_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Scheduler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Date = table.Column<DateTime>(type: "Date", nullable: false),
+                    StartTime = table.Column<string>(maxLength: 5, nullable: true),
+                    EndTime = table.Column<string>(maxLength: 5, nullable: true),
+                    UsersId = table.Column<int>(nullable: false),
+                    Max = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scheduler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Scheduler_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    UsersId = table.Column<int>(nullable: true),
+                    RolesId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsersRoles_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UsersRoles_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersToken",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UsersId = table.Column<int>(nullable: false),
+                    Token = table.Column<string>(nullable: true),
+                    ExpiresTo = table.Column<DateTime>(nullable: true),
+                    RefreshToken = table.Column<string>(nullable: true),
+                    RefreshExpiresTo = table.Column<DateTime>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersToken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsersToken_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clinics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ClinicNo = table.Column<string>(maxLength: 10, nullable: true),
+                    ClinicName = table.Column<string>(maxLength: 20, nullable: true),
+                    SchedulerId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clinics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clinics_Scheduler_SchedulerId",
+                        column: x => x.SchedulerId,
+                        principalTable: "Scheduler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Gid = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 30, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Comment = table.Column<string>(nullable: true),
+                    SchedulerId = table.Column<int>(nullable: false),
+                    PatientsId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_Patients_PatientsId",
+                        column: x => x.PatientsId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Order_Scheduler_SchedulerId",
+                        column: x => x.SchedulerId,
+                        principalTable: "Scheduler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppsApiCollection_AppsId",
+                table: "AppsApiCollection",
+                column: "AppsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clinics_SchedulerId",
+                table: "Clinics",
+                column: "SchedulerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_PatientsId",
+                table: "Order",
+                column: "PatientsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_SchedulerId",
+                table: "Order",
+                column: "SchedulerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolesApps_AppsId",
+                table: "RolesApps",
+                column: "AppsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolesApps_RolesId",
+                table: "RolesApps",
+                column: "RolesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scheduler_UsersId",
+                table: "Scheduler",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersRoles_RolesId",
+                table: "UsersRoles",
+                column: "RolesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersRoles_UsersId",
+                table: "UsersRoles",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersToken_UsersId",
+                table: "UsersToken",
+                column: "UsersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppsApiCollection");
+
+            migrationBuilder.DropTable(
+                name: "Clinics");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "RolesApps");
+
+            migrationBuilder.DropTable(
+                name: "UsersRoles");
+
+            migrationBuilder.DropTable(
+                name: "UsersToken");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.DropTable(
+                name: "Scheduler");
+
+            migrationBuilder.DropTable(
+                name: "Apps");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Vue.Core.Data/Migrations/20190925065644_add.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vue.Core.Data.Migrations
+{
+    public partial class add : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RefreshTimes",
+                table: "UsersToken",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UsersToken",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshTimes",
+                table: "UsersToken");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UsersToken");
+        }
+    }
+}
